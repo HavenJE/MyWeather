bbdc48b424fd0f27d7683264cb903596fe58354a
haven <haven.eldebaque@gmail.com>
2021-10-20 14:31:33 +1000

    initial style pushed to LocationsAndUsers_models

---
+++ db/migrate/20211020035700_create_locations.rb
@@ -0,0 +1,12 @@
+class CreateLocations < ActiveRecord::Migration[6.1]
+  def change
+    create_table :locations do |t|
+      t.text :title
+      t.text :content
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
